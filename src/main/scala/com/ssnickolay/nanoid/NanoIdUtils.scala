95db1b7ddc8aaea5a83f91d6213ba27ae6f264e1
Nikolay Sverchkov <ssnikolay@gmail.com>
2018-03-11 21:00:25 +0300

    Base implementation of NanoIdUtils

--- src/main/scala/com/ssnickolay/nanoid/NanoIdUtils.scala
+++ src/main/scala/com/ssnickolay/nanoid/NanoIdUtils.scala
@@ -1,7 +1,6 @@
 package com.ssnickolay.nanoid
 
 import scala.util.{Random}
-import scala.math._
 
 object NanoIdUtils {
   lazy val DefaultNumberGenerator = new scala.util.Random()
@@ -15,28 +14,27 @@ object NanoIdUtils {
     generate(DefaultNumberGenerator, DefaultAlphabet, DefaultSize)
 
   def generate(random: Random, alphabet: Array[Char], size: Int): String = {
-    val mask: Int = (2 << Math.floor(Math.log(alphabet.length - 1) / Math.log(2)).asInstanceOf[Int]) - 1;
-    val step: Int = Math.ceil(1.6 * mask * size / alphabet.length).asInstanceOf[Int];
+    val mask: Int = (2 << Math.floor(Math.log(alphabet.length - 1) / Math.log(2)).asInstanceOf[Int]) - 1
+    val step: Int = Math.ceil(1.6 * mask * size / alphabet.length).asInstanceOf[Int]
     val builder = new StringBuilder()
+    var result: String = ""
 
-    while (true) {
+    while (result == "") {
+      var bytes: Array[Byte] = new Array[Byte](step)
+      random.nextBytes(bytes)
 
-      val bytes: Array[Byte] = new Byte[step];
-      random.nextBytes(bytes);
-
-      for (int i = 0; i < step; i++) {
-
-        final int alphabetIndex = bytes[i] & mask;
+      for (i <- 0 until step) {
+        val alphabetIndex: Int = bytes(i) & mask
 
         if (alphabetIndex < alphabet.length) {
-          idBuilder.append(alphabet[alphabetIndex]);
-          if (idBuilder.length() == size) {
-            return idBuilder.toString();
+          builder.append(alphabet(alphabetIndex))
+          if (builder.length() == size) {
+            result = builder.toString()
           }
         }
-
       }
-
     }
+
+    result
   }
 }
