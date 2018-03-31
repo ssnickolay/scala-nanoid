1dd0371081dc26f86360e27055da6aac10b36766
Nikolay Sverchkov <ssnikolay@gmail.com>
2018-03-31 20:31:20 +0300

    [JVM] Add test about passing custom random

--- shared/src/test/scala/com/ssnickolay/nanoid/NanoIdUtilsSpec.scala
+++ shared/src/test/scala/com/ssnickolay/nanoid/NanoIdUtilsSpec.scala
@@ -9,7 +9,7 @@ class NanoIdUtilsSpec extends UnitSpec {
   }
 
   it should "generate 100k random and uniq IDs" in {
-    var ids =
+    val ids =
       Stream
         .continually(NanoIdUtils.generate)
         .take(100000)
@@ -22,5 +22,23 @@ class NanoIdUtilsSpec extends UnitSpec {
     1.to(1000).foreach( size =>
       assert(size === NanoIdUtils.generate(size).length)
     )
+  }
+
+  it should "generate with seeded Random" in {
+    val random = new scala.util.Random(12345)
+    var alphabet = NanoIdUtils.DefaultAlphabet
+    var size = 21
+    var exectedIDs = Seq(
+      "kutqLNv1wDmIS56EcT3j7",
+      "U497UttnWzKWWRPMHpLD7",
+      "7nj2dWW1gjKLtgfzeI8eC",
+      "I6BXYvyjszq6xV7L9k2A9",
+      "uIolcQEyyQIcn3iM6Odoa"
+    )
+
+    exectedIDs.foreach(exected => {
+      var actual = NanoIdUtils.generate(random, alphabet, size);
+      assert(exected === actual)
+    })
   }
 }
