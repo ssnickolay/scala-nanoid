a95a86387ef42691a801dfb949a0669d9c2a600f
Nikolay Sverchkov <ssnikolay@gmail.com>
2018-04-01 18:58:09 +0300

    [JS] Allow to replace the default safe random generator

---
+++ js/src/test/scala/com/ssnickolay/nanoid/NanoidUtilsJSSpec.scala
@@ -0,0 +1,19 @@
+package com.ssnickolay.nanoid
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+
+@ScalaJSDefined
+object FakeRandom extends js.Object {
+  def build(size: Int): Array[Int] =
+    1.to(size).map(_ => 123).toArray
+}
+
+class NanoIdUtilsJSSpec extends UnitSpec {
+  it should "generate with seeded JS Random" in {
+    var alphabet = NanoIdUtils.DefaultAlphabet
+    var size = 21
+
+    assert(NanoIdUtils.generate((i) => FakeRandom.build(i), alphabet, size) === "VVVVVVVVVVVVVVVVVVVVV")
+  }
+}
--- shared/src/main/scala/com/ssnickolay/nanoid/NanoIdUtils.scala
+++ shared/src/main/scala/com/ssnickolay/nanoid/NanoIdUtils.scala
@@ -40,4 +40,28 @@ object NanoIdUtils {
 
     result
   }
+
+  def generate(random: Int => Array[Int], alphabet: Array[Char], size: Int): String = {
+    val mask: Int = (2 << Math.floor(Math.log(alphabet.length - 1) / Math.log(2)).asInstanceOf[Int]) - 1
+    val step: Int = Math.ceil(1.6 * mask * size / alphabet.length).asInstanceOf[Int]
+    val builder = new StringBuilder()
+    var result: String = ""
+
+    while (result == "") {
+      var bytes = random(step)
+
+      for (i <- 0 until step) {
+        val alphabetIndex: Int = bytes(i) & mask
+
+        if (alphabetIndex < alphabet.length) {
+          builder.append(alphabet(alphabetIndex))
+          if (builder.length() == size) {
+            result = builder.toString()
+          }
+        }
+      }
+    }
+
+    result
+  }
 }
